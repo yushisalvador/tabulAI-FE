ffac2d08672bc68049af93875df6b2f1e9976de9
yccjs <yccjsalvador@gmail.com>
2022-10-16 16:16:58 +0900

    feat: try sending get request to Backend

--- src/components/QuestionBox.tsx
+++ src/components/QuestionBox.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import {
   Box,
   Input,
@@ -6,16 +7,30 @@ import {
   FormControl,
   FormLabel,
   Button,
-  Container,
 } from "@chakra-ui/react";
 
 export default function QuestionBox() {
+  const [question, setQuestion] = useState("");
+  const getReq = async () => {
+    const req = await axios.get("https://pandai-2.cathalweakliam.repl.co");
+    console.log(req);
+  };
+
+  useEffect(() => {
+    getReq();
+  }, []);
+
   return (
     <Flex direction="column" justify="center" align="center" mt="10">
       <Box>
         <FormControl>
           <FormLabel> Question</FormLabel>
-          <Input placeholder="type your question" />
+          <Input
+            placeholder="type your question"
+            onChange={(e) => {
+              setQuestion(e.target.value);
+            }}
+          />
           <Button
             type="submit"
             variant="solid"
